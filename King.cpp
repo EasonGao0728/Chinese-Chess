c0ff6780412bbc950695ec8d93b0936e8c8c379e
Eason <xichengao@foxmail.com>
2026-06-04 16:13:26 +0800

    King的照将判断

--- King.cpp
+++ King.cpp
@@ -11,8 +11,8 @@ bool King::is_legal(pos start,pos end,const Board& chessBoard)const
 {
     bool check_legal_a=false;
     bool check_legal_b=false;
-    bool check_legal_c=false;
-    if(Chess::gettype()=="r")//根据棋子所在阵营判断走棋范围是否合法
+    bool is_direct=false;
+    if(Chess::getside()=='r')//根据棋子所在阵营判断走棋范围是否合法
     {
         if(end.x<3||end.x>5||end.y>2)
         {
@@ -20,7 +20,7 @@ bool King::is_legal(pos start,pos end,const Board& chessBoard)const
             return check_legal_a;
         }
     }
-    else if(Chess::gettype()=="b")
+    else if(Chess::getside()=='b')
     {
         if(end.x<3||end.x>5||end.y<7)
         {
@@ -28,36 +28,56 @@ bool King::is_legal(pos start,pos end,const Board& chessBoard)const
             return check_legal_a;
         }
     }
-    if(chessBoard.is_exist(end)==1)//判断是否遇到棋子
+    if(chessBoard.is_exist(end)==1)//判断是否遇到敌方棋子
         {
-            if(chessBoard.get_chess(end).gettype()!=Chess::gettype())
+            if(chessBoard.get_chess(end).getside()!=Chess::getside())
             {
                 check_legal_a=true;
             }
         }
-    if((end.y-start.y)*(end.y-start.y)+(end.x-start.x)*(end.x-start.x)==1)//判断是否一次走一格
-    {
-        check_legal_b==true;
-    }
-    bool is_direct=false;
-    for(int i=2;i>=0;--i)
-    {
-        pos temp(end.x,abs(9-end.y-i));
-        if(chessBoard.is_exist(temp)==1)
-        {
-         if(chessBoard.get_chess(temp).gettype()=="King")
-        {
-            if(is_direct==0)
-            {
-                is_direct=1;
-            }
-
-        }
         else
         {
-            break;
+            check_legal_a=true;
         }
+    if((end.y-start.y)*(end.y-start.y)+(end.x-start.x)*(end.x-start.x)==1)//判断是否一次走一格
+    {
+        check_legal_b=true;
+    }
+    if(Chess::getside()=='r')//照将判断
+    {
+        for(int i=end.y+1;i<=9;++i)
+        {
+            pos temp(end.x,i);
+            if(chessBoard.is_exist(temp))
+            {
+                if(chessBoard.get_chess(temp).gettype()=="King")
+                {
+                    is_direct=1;
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
-    return check_legal_a&&check_legal_b;
+    else if(Chess::getside()=='b')
+    {
+        for(int i=end.y-1;i>=0;--i)
+        {
+            pos temp(end.x,i);
+            if(chessBoard.is_exist(temp))
+            {
+                if(chessBoard.get_chess(temp).gettype()=="King")
+                {
+                    is_direct=1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+    return check_legal_a&&check_legal_b&&is_direct;
 }
