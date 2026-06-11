daf13cdc26fb105e3b9d9ecc562562e9c17e9768
Eason <xichengao@foxmail.com>
2026-06-11 17:05:00 +0800

    Move完善

--- Move/Move.cpp
+++ Move/Move.cpp
@@ -7,19 +7,54 @@ Move::Move(pos start_pos, pos end_pos, const Board& chessBoard, int step)
     step_count=step;
     acting_piece=chessBoard.get_chess(start);
     char side_1=chessBoard.get_chess(start).getside();
-    char side_2=chessBoard.get_chess(end).getside();
     check_eat=0;
     check_king=0;
     side=side_1;
-    if(side_2!=side_1)
+    if(chessBoard.is_exist(end))
+    {
+      char side_2=chessBoard.get_chess(end).getside();
+      if(side_2!=side_1)
     {
         check_eat=1;
         eaten_piece=chessBoard.get_chess(end);
     }
-    else
+      else
     {
         eaten_piece=Chess();
     }
+    }
+    else{
+        eaten_piece=Chess();
+    }
+
     //TO DO: 调用Game或者RuleChecker等其他类的一些辅助函数实现将军的判断
     //check_king=
+}
+pos Move::get_start()
+{
+    return start;
+}
+pos Move::get_end()
+{
+    return end;
+}
+const Chess& Move::get_acting_chess()
+{
+    return acting_piece;
+}
+const Chess& Move::get_eaten_chess()
+{
+    return eaten_piece;
+}
+char Move::get_side()
+{
+    return side;
+}
+bool Move::get_check_eat()
+{
+    return check_eat;
+}
+bool Move::get_check_king()
+{
+    return check_king;
 }
